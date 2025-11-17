db1a81efe2eeec2c58d9c02cdf04f7f0ee1d2426
Muhammad Qasim <qasimmuhmmed0@gmail.com>
2025-11-17 09:12:11 +0500

    input handling to ask user create file and enter text

--- 2.c
+++ 2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main(){
 	char filename[100];
+	char input[50];
 	
 	printf("Enter File Name : ");
 	scanf("%s",filename);
 	
     FILE *fptr ;
-    
     fptr = fopen(filename,"w");
     
+    printf("\nEnter Your Input : ");
+    scanf(" %[^\n]",input);
+    fprintf(fptr,"%s",input);
+
     fclose(fptr);
     
     return 0;
